7e22587b92e42e7c8ebc60d40aa0b04a59c64c60
TigerPapaDev <jnmcluo@gmail.com>
2016-12-11 23:48:52 +0800

    1st example

--- basic/src/test/java/com/mondoev/springstudy/basic/test/ApplicationTest.java
+++ basic/src/test/java/com/mondoev/springstudy/basic/test/ApplicationTest.java
@@ -1,28 +1,19 @@
 package com.mondoev.springstudy.basic.test;
 
 import com.monodev.springstudy.basic.beans.Car;
-import com.monodev.springstudy.basic.beans.AppConfig;
-import com.monodev.springstudy.basic.beans.Engine;
-import com.monodev.springstudy.basic.beans.Wheel;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by jnmcl on 2016/12/10.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class ApplicationTest {
-
-
-    public  void beanFactoryTest(){
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
+    @Test
+    public  void beanFactoryTest(){ 
     }
 }
